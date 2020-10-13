8da0342eedc60eea5a2fc2a073d1c62c0e0c6cc6
cp6 <corbin@corbpie.com>
2020-10-13 16:12:05 +1100

    Added working examples
    Added some working examples

--- example.php
+++ example.php
@@ -1,5 +1,29 @@
 <?php
 require_once('bunnyAPI.php');
 $bunny = new BunnyAPI();
-$bunny->apiKey('8561609c-681d-6q05-ck9m-34810be1542abaa4d8-f8de-2208-ff38-26c58dadb544');
-echo $bunny->listPullZones();//Returns data for all pull zones on account
+//Make sure API_KEY is set at line 10 bunnyAPI.php
+
+echo $bunny->listPullZones();//Returns data for all Pull zones on account
+
+echo $bunny->listStorageZones();//Returns data for all Storage zones on account
+
+$bunny->zoneConnect('homeimagebackups', '');//Create connection to 'homeimagebackups'
+//Access key can be set or left empty to which it will auto fetch from a listStorageZones() call
+
+//List folders for storage zone 'homeimagebackups'
+echo $bunny->listFolders();
+
+//Create a new folder called pets
+echo $bunny->createFolder('pets');
+
+//Upload file (fluffy.jpg) into pets folder
+echo $bunny->uploadFile('fluffy.jpg', '/pets/fluffy.jpg');
+
+//Rename fluffy.jpg to fluffy_young.jpg
+echo $bunny->rename('pets/', 'fluffy.jpg', 'pets/', 'fluffy_young.jpg');
+
+//Delete fluffy_young.jpg
+echo $bunny->deleteFile('pets/fluffy_young.jpg');
+
+//Delete pets folder (only works if folder empty)
+echo $bunny->deleteFolder('pets/');
