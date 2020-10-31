81fc12487b52e58e65b05bff1d006956dd194e56
cp6 <corbin@corbpie.com>
2020-10-31 18:36:46 +1100

    Updated examples
    Updated working examples

--- example.php
+++ example.php
@@ -20,10 +20,14 @@ echo $bunny->createFolder('pets');
 echo $bunny->uploadFile('fluffy.jpg', '/pets/fluffy.jpg');
 
 //Rename fluffy.jpg to fluffy_young.jpg
-echo $bunny->rename('pets/', 'fluffy.jpg', 'pets/', 'fluffy_young.jpg');
+echo $bunny->renameFile('pets/', 'fluffy.jpg', 'fluffy_young.jpg');//Renames pets/fluffy.jpg as pets/fluffy_young.jpg
+
+//Move a file
+echo $bunny->moveFile('pets/', 'fluffy_young.jpg', 'pets/puppy_fluffy/');//Moves pets/fluffy_young.jpg to pets/puppy_fluffy/fluffy_young.jpg
 
 //Delete fluffy_young.jpg
-echo $bunny->deleteFile('pets/fluffy_young.jpg');
+echo $bunny->deleteFile('pets/puppy_fluffy/fluffy_young.jpg');
 
-//Delete pets folder (only works if folder empty)
+//Delete folders (only works if folder empty)
+echo $bunny->deleteFolder('pets/puppy_fluffy/');
 echo $bunny->deleteFolder('pets/');
